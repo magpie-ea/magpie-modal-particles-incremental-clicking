c60b7b2cc94c89ad812dfa922831d363cd6331ec
michael-franke <mchfranke@gmail.com>
2019-11-14 21:41:03 +0100

    added data and exploration second pilot

--- data/01_pilot/data-exploration.r
+++ data/01_pilot/data-exploration.r
@@ -1,5 +1,46 @@
 library(tidyverse)
 
+d_unrel = read_csv('data_19-11-2019_unreliable.csv') %>% 
+  filter(
+  # discard training data
+  trial_name != "poor_folks_visual_world_training",
+  # remove test trial by FM
+  submission_id != 380,
+  # discard fllers
+  condition != "filler"
+) %>% 
+  separate(
+    col = pictureChoices,
+    into = c("choice_initial", "choice_MP", "choice_final"),
+    sep = "\\|"
+  ) %>% 
+  select(
+    submission_id, trial_number, DP, 
+    item_id, target, competitor,
+    choice_initial,
+    choice_MP,
+    choice_final,
+    comments
+  ) %>% 
+  pivot_longer(
+    cols = contains("choice"),
+    names_to = "position",
+    values_to = "choice"
+  ) %>% 
+  mutate(
+    position = str_remove(position, "choice_") %>% 
+      factor(ordered = T, levels = c("initial", "MP", "final")),
+    choice_type = case_when(
+      choice == target ~ "target",
+      choice == competitor ~ "competitor",
+      TRUE ~ "distractor"
+    ) %>% 
+      factor(ordered = T, levels = c("target", "competitor", "distractor"))
+  )
+  
+
+
+
 d = read_csv('data_19-10-2019.csv') %>% 
   filter(
     # discard training data
@@ -7,7 +48,7 @@ d = read_csv('data_19-10-2019.csv') %>%
     # remove incomplete data sets (coding mistake!)
     submission_id > 22,
     # discard fllers
-    condition == "reliable"
+    condition != "filler"
   ) %>% 
   separate(
     col = pictureChoices,
@@ -38,11 +79,23 @@ d = read_csv('data_19-10-2019.csv') %>%
       factor(ordered = T, levels = c("target", "competitor", "distractor"))
   )
 
-d$comments %>% unique()
+d$group = "reliable"
+d_unrel$group = "unreliable"
+d_complete = rbind(d, d_unrel)
 
 d %>% ggplot(aes(x = choice_type, fill = choice_type)) +
   geom_bar() +
   facet_grid(DP ~ position) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  labs(x = "", y = "", title = "Counts of picture choices during incremental forced-choice task") +
+  theme(axis.text.x = element_text(angle = 40, hjust = 1))
+ 
+d_complete %>% 
+  filter( position == "MP") %>% 
+  ggplot(aes(x = choice_type, fill = choice_type)) +
+  geom_bar() +
+  facet_grid(DP ~ group) +
   theme_minimal() +
   theme(legend.position = "none") +
   labs(x = "", y = "", title = "Counts of picture choices during incremental forced-choice task") +
--- data/01_pilot/notes.md
+++ data/01_pilot/notes.md
@@ -1,6 +1,16 @@
+# first pilot -- reliable
+
 - pilot run on October 19th on Prolific (study name "Bilderbuch (incremental MP)")
 - first submission_id is MF testing
 - next 21 submissions are from erroneous posting where not all trials were displayed (failure of MF after debugging)
 - finally collected another 20 VPs
 - drafted were participants with German as first language and minimum 70% acceptance rate
 - paid 1.20 pounds (for approximately 9 minutes of work)
+
+# second pilot -- unreliable
+
+- run on November 14th
+- first submission by MF for testing
+- 20 participants (disjoint from first run)
+- drafted participants with German first language
+- paid 1.25
